412ad5984ac4e027623a5973f764d7cc0a7f9366
Ben Hall <benhall2121@yahoo.com>
2013-02-25 16:10:03 -0700

    benhall2121@yahoo.com initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DKSSOQ::Application.config.secret_token = 'b605c9899074a25d49b0cce593504b3ade12a1c9ae6933ab34eebe01edd36975b51844ec9569fb11fd8c624259ea9687b406b308e59a8d2a07bc474f94832501'
